3a050de75e56f3795c1998d07c806051746a465d
Yamini Deore <yamini.deore_it24@pccoer.in>
2026-04-09 21:32:04 +0530

    Create length_of_LL.java

---
+++ length_of_LL.java
@@ -0,0 +1,22 @@
+/*
+Definition for Linked List
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}
+*/
+class Solution {
+    public int getCount(Node head) {
+        // code here
+        Node curr = head;
+        int count = 0 ;
+        
+        while(curr != null)
+        {
+            count++;
+            curr = curr.next ;
+        }
+        return count ;
+    }
+}
